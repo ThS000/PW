b9aa4395ccbe6744af2e8994ffc8370ae56d7c19
Thiago Saraiva <thiagosaraiva.dev.br@gmail.com>
2026-06-21 14:18:25 -0300

    Objetos

---
+++ OBJETOS/Exercícios/EX2/scrpit.js
@@ -0,0 +1,17 @@
+const task = {
+    id: 301,
+    title:  "Treino de LLM",
+    status: "pendente"
+}
+
+console.log(task.title);
+
+console.log(task.status);
+
+task.priority = "alta";
+
+console.log(task.priority);
+
+task.status = "em_execucao";
+
+console.log(task);
