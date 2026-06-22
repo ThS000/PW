9162ac35d0377ac685524f12667d32989f103d35
Thiago Saraiva <thiagosaraiva.dev.br@gmail.com>
2026-06-22 15:49:02 -0300

    Aluno aprovado com forEach

---
+++ ARRAYS/Exercícios/forEach/EX4/script.js
@@ -0,0 +1,15 @@
+const alunos = [
+  { nome: 'Ana', nota: 8 },
+  { nome: 'Bruno', nota: 5 },
+  { nome: 'Carla', nota: 9 },
+  { nome: 'Diego', nota: 6 }
+];
+
+alunos.forEach((aluno) => {
+    if (aluno.nota >= 7){
+        console.log("Aluno: " + aluno.nome +  " aprovado");
+    }else {
+        console.log("Aluno: " + aluno.nome + " reprovado");
+    }
+});
+
