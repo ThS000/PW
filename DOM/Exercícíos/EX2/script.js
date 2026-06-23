5327f0a8068233cefd4597a78d7bc4d40b20f057
Thiago Saraiva <thiagosaraiva.dev.br@gmail.com>
2026-06-22 21:45:18 -0300

    Básico de DOM

--- DOM/Exercícíos/EX2/script.js
+++ DOM/Exercícíos/EX2/script.js
@@ -1,14 +1,3 @@
-const input = document.querySelector("#inputTarefa");
-const button = document.querySelector("#btnAdicionar");
-const lista = document.querySelector("#lista");
+const ul = document.querySelector("#container");
 
-button.addEventListener ("click", () => {
-  const valor = input
-
-   const li = document.createElement("li");
-  li.textContent = valor;
-
-  lista.appendChild(li);
-
-  input.value = "";
-});
+ul.innerHTML = "<li>Ana</li><li>Bruno</li><li>Carla</li>";
